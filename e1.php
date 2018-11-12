b7dcc0962edeb7cadc73998781bf1c300cf7dc23
Miguel Martinez <miguelitalia@hotmail.com>
2018-11-12 11:46:50 +0000

    added practical 8 exercise 1

---
+++ e1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $colours = array("white", "green", "red");
+    
+    foreach ($colours as $value) {
+        echo "$value ";
+    }
+    
+    foreach ($colours as $value) {
+        echo " <li>$value</li>";
+    }
+    
+    ?>
+    
+    
+</body>
+</html>
